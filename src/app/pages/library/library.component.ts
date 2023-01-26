1478a11691ed399198fdfaed5d15ad2458cbdc0a
Rmarieta <raphael.marietan@epfl.ch>
2023-01-26 10:53:04 +0100

    library

--- src/app/pages/library/library.component.ts
+++ src/app/pages/library/library.component.ts
@@ -64,5 +64,56 @@ export class LibraryComponent {
       height: 250,
       ownerName: 'Pharalé',
     },
+    {
+      id: 6,
+      name: 'TUSEK',
+      description:
+        'Amazing playlist listing all the latest songs that have been released in Rap FR',
+      image:
+        'https://raw.githubusercontent.com/Rmarieta/SpotifyAngular/main/src/assets/static/playlist_1.png',
+      width: 250,
+      height: 250,
+      ownerName: 'Pharalé',
+    },
+    {
+      id: 7,
+      name: 'TATATA',
+      description: 'Amazing reggaeton playlist',
+      image:
+        'https://raw.githubusercontent.com/Rmarieta/SpotifyAngular/main/src/assets/static/playlist_2.png',
+      width: 250,
+      height: 250,
+      ownerName: 'Pascalia Kyrimis',
+    },
+    {
+      id: 8,
+      name: 'MELODIC',
+      description: 'Playlist with melodic techno',
+      image:
+        'https://raw.githubusercontent.com/Rmarieta/SpotifyAngular/main/src/assets/static/playlist_3.png',
+      width: 250,
+      height: 250,
+      ownerName: 'Pharalé',
+    },
+    {
+      id: 9,
+      name: 'DRILLITY DRILL',
+      description: 'Drill music, FR & UK',
+      image:
+        'https://raw.githubusercontent.com/Rmarieta/SpotifyAngular/main/src/assets/static/playlist_4.png',
+      width: 250,
+      height: 250,
+      ownerName: 'Pharalé',
+    },
+    {
+      id: 10,
+      name: 'MGNTA',
+      description: 'Old school magenta stage type of music',
+      image:
+        'https://raw.githubusercontent.com/Rmarieta/SpotifyAngular/main/src/assets/static/playlist_5.png',
+      width: 250,
+      height: 250,
+      ownerName: 'Pharalé',
+    },
   ];
 }
